de9362f2bd3d20ccad148133b101f15b83bf6376
nhinguyen <nguyenhis@gmail.com>
2023-11-29 19:25:06 +0200

    13 2

--- ex11to13/13.py
+++ ex11to13/13.py
@@ -1,3 +1,4 @@
+#1
 import json
 
 import math
@@ -68,3 +69,42 @@ def page_not_found(error_code):
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
+
+#2
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+db_connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='dbuser',
+    password='pass_word'
+)
+
+def get_airport_info_from_db(icao_code):
+    cursor = db_connection.cursor()
+    sql = "SELECT ident, name, iso_country FROM airport WHERE ident = %s"
+    cursor.execute(sql, (icao_code,))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+@app.route('/airport/<icao_code>', methods=['GET'])
+def get_airport_info(icao_code):
+    airport_info = get_airport_info_from_db(icao_code.upper())
+
+    if airport_info:
+        response = {
+            "ICAO": airport_info[0],
+            "Name": airport_info[1],
+            "Location": airport_info[2],
+        }
+        return f'{{"ICAO":"{response["ICAO"]}", "Name":"{response["Name"]}", "Location":"{response["Location"]}"}}'
+    else:
+        return '{"error": "Airport not found"}', 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
